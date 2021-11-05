1ff5a43e5104da44847fcc68530936d8a78013c8
Jake Lanning <42739838+jalanning@users.noreply.github.com>
2021-11-05 00:45:09 -0400

    complete challenge
    challenge 1 on freecodecamp's data analysis course

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,7 +2,22 @@ import numpy as np
 
 def calculate(list):
 
+    if len(list) != 9:
+      raise ValueError("List must contain nine numbers.")
+    
+    a = np.array(list).reshape(3,3)
 
+    def getVals(npfunc):
+      return [npfunc(a, axis=0).tolist(), npfunc(a, axis=1).tolist(), npfunc(a)]
+
+    calculations = {
+      'mean': getVals(np.mean),
+      'variance': getVals(np.var),
+      'standard deviation': getVals(np.std),
+      'max': getVals(np.max),
+      'min': getVals(np.min),
+      'sum': getVals(np.sum)
+    }
 
 
     return calculations
